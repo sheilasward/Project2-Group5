c5396064eb9b71ccde0f40c8f871eeb4880f15d0
Hardin Ross <38013016+heross91@users.noreply.github.com>
2018-09-21 11:33:28 -0400

    Update students.js
    changed to JS

--- models/students.js
+++ models/students.js
@@ -1,5 +1,17 @@
-CREATE TABLE students (
-    column1 name,
-    column2 course,
-    column3 credits,
-);
+module.exports = function(sequelize, DataTypes) {
+var Tablename = sequelize.define("Tablename", {
+// The email cannot be null, and must be a proper email before creation
+field1 : {
+type: DataTypes.STRING,
+allowNull: false,
+unique: true,
+validate: {
+/* if needed*/
+}
+},
+// The password cannot be null
+field2: {
+type: DataTypes.STRING,
+allowNull: false
+}
+});
