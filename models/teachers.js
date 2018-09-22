1bbe1480944d70ecbf796ed476a2a539d6747a2c
sheilasward <ssward1@gtcc.edu>
2018-09-21 20:35:01 -0400

    fix for teacher.js

--- models/teachers.js
+++ models/teachers.js
@@ -1,32 +1,22 @@
 module.exports = function(sequelize, DataTypes) {
-    var Teacher = sequelize.define("Teacher", {
-        lastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1 - 100]
-             }
-        },
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1 - 100]
-            }
-        },
-        designation: {
-            type: DataTypes.STRING,
-        }
-    });
-
-    Teacher.associate = function(models) {
-        Teacher.hasMany(models.Classes, {
-            onDelete: "cascade",
-            constraints: true,
-            foreignKey: {
-                name: "teacherId"    
-            }
-        });
-    };
-    return Example;
-  };
+  var Teacher = sequelize.define("Teacher", {
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1 - 100]
+      }
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1 - 100]
+      }
+    },
+    designation: {
+      type: DataTypes.STRING
+    }
+  });
+  return Teacher;
+};
