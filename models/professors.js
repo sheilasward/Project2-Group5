32e87824a08809987817a2ec6f708b6b4f0d6476
sheilasward <ssward1@gtcc.edu>
2018-09-22 13:57:03 -0400

    set up all tables

---
+++ models/professors.js
@@ -0,0 +1,27 @@
+module.exports = function(sequelize, DataTypes) {
+  var Professor = sequelize.define("Professor", {
+    pname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    office: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    dept: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  Professor.associate = function(models) {
+    Professor.hasMany(models.Class, {
+      onDelete: "cascade",
+      constraints: true,
+      foreignKey: {
+        name: "pnum"
+      }
+    });
+  };
+
+  return Professor;
+};
