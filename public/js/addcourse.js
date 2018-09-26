0869393bb9a579f50c963ef9d98e1508c7081641
unknown <sundaresh.sankaran@gmail.com>
2018-09-26 19:04:35 -0400

    Add course added

--- public/js/addcourse.js
+++ public/js/addcourse.js
@@ -1,16 +1,21 @@
-// Getting references to our form and input
-var addCourseForm = $("form#addcourse");
-var cnameInput = $("input#cname-input");
-var creditInput = $("input#credit-input");
-
+$("#addcourse").on("click", function(event) {
 // When the submit button is clicked, we validate the inputs - the course name should not be blank
-addCourseForm.on("submit", function(event) {
-  console.log("Inside Add Course");
+
   event.preventDefault();
+  // Getting references to our form and input
+  var addCourseForm = $("form#addcourse");
+  var cnameInput = $("input#cname-input");
+  var creditInput = $("input#credit-input");
+  console.log("Inside addcourse.js");
+
+  alert("Inside");
+  console.log("Inside Add Course");  
   var userData = {
     cname: cnameInput.val().trim(),
     credits: creditInput.val().trim()
   };
+
+  console.log(userData);
 
   if (!userData.cname) {
     return;
@@ -19,6 +24,7 @@ addCourseForm.on("submit", function(event) {
   addCourse(userData);
   cnameInput.val("");
   creditInput.val("");
+  
 });
 // Does a post to the addcourse route. If successful, we are redirected to the main page
 // Otherwise we log any errors
@@ -26,12 +32,14 @@ addCourseForm.on("submit", function(event) {
 function addCourse(userData) {
   $.post("/api/addcourse", userData)
     .then(function(data) {
-      window.location.replace(data);
+      console.log(data);
+      // window.location.replace(data);
     })
     .catch(handleAddCourseErr);
-}
+};
 
 function handleAddCourseErr(err) {
+  console.log(err.responseJSON);
   $("#alert .msg").text(err.responseJSON);
   $("#alert").fadeIn(500);
 }
