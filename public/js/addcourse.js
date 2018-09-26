a9a8803c3b54fff141bd31a50f907b62b92a63b4
unknown <sundaresh.sankaran@gmail.com>
2018-09-26 19:55:54 -0400

    Add a course

--- public/js/addcourse.js
+++ public/js/addcourse.js
@@ -3,28 +3,35 @@ $("#addcourse").on("click", function(event) {
 
   event.preventDefault();
   // Getting references to our form and input
-  var addCourseForm = $("form#addcourse");
+
   var cnameInput = $("input#cname-input");
   var creditInput = $("input#credit-input");
+  var descInput = $("input#desc-input");
+  var deptInput = $("#dept-input");
+  var preInput = $("#pre-input");
+
   console.log("Inside addcourse.js");
 
   alert("Inside");
-  console.log("Inside Add Course");  
+  console.log("Inside Add Course");
+  console.log(deptInput);
   var userData = {
-    cname: cnameInput.val().trim(),
-    credits: creditInput.val().trim()
+    courseName: cnameInput.val().trim(),
+    credits: creditInput.val().trim(),
+    courseDesc: descInput.val().trim(),
+    dept: deptInput.val(),
+    prerequisite: preInput.val()
   };
 
   console.log(userData);
 
-  if (!userData.cname) {
+  if (!userData.courseName) {
     return;
   }
   // If we have our inputs in proper order, run the signUpUser function
   addCourse(userData);
   cnameInput.val("");
   creditInput.val("");
-  
 });
 // Does a post to the addcourse route. If successful, we are redirected to the main page
 // Otherwise we log any errors
