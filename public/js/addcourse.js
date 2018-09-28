d8aad1eb8fd3f6e366e746e3ea862ca8249eb556
sheilasward <ssward1@gtcc.edu>
2018-09-28 15:00:24 -0400

    working on searchclasses and show-classes

--- public/js/addcourse.js
+++ public/js/addcourse.js
@@ -11,13 +11,13 @@ $("#addcourse").on("click", function(event) {
   var preInput = $("#pre-input");
 
   console.log("Inside addcourse.js");
-
+  
   alert("Inside");
   console.log("Inside Add Course");
-  console.log(deptInput);
+  console.log("deptInput = " + deptInput);
   var userData = {
     courseName: cnameInput.val().trim(),
-    credits: creditInput.val().trim(),
+    credits: creditInput.val(),
     courseDesc: descInput.val().trim(),
     dept: deptInput.val(),
     prerequisite: preInput.val()
@@ -43,10 +43,9 @@ function addCourse(userData) {
       // window.location.replace(data);
     })
     .catch(handleAddCourseErr);
-};
+}
 
 function handleAddCourseErr(err) {
-  console.log(err.responseJSON);
   $("#alert .msg").text(err.responseJSON);
   $("#alert").fadeIn(500);
 }
