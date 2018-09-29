3006b4414006c6f3fe463fbafb36ece14d791e36
Onray <mcintyrehh@gmail.com>
2018-09-29 01:35:46 -0400

    class searching works

--- public/js/searchclass.js
+++ public/js/searchclass.js
@@ -11,25 +11,47 @@ $("#search-class").on("click", function(event) {
     term: term,
     dept: dept
   };
-
   console.log("body = " + JSON.stringify(body));
-
-  $.get("/api/class-search", function(data) {
-    console.log(data);
+  $.post("/api/class-search", {
+    term: body.term,
+    dept: body.dept
+  }).then(function(data) {
+    console.log("data: " + JSON.stringify(data));
+    console.log(data.length);
     renderClasses(data);
   });
 });
 
-function renderClasses(page, data) {
-  if (data.length !== 0) {
-    page.render("show-classes");
-    $("#classes").empty();
-    for (var i = 0; i < data.length; i++) {
-      var div = $("<div>");
-      div.append(
-        `<p>${data[i].term} ${data[i].courseName} ${data[i].dept} </p>`
-      );
-      $("#classes").append(div);
-    }
+function renderClasses(object) {
+  var classTable = `<table class="table classes-table">
+  <thead>
+    <tr>
+      <th scope="col">Class Name</th>
+      <th scope="col">Professor</th>
+      <th scope="col">Start Time</th>
+      <th scope="col">Days/Week</th>
+      <th scope="col">Building</th>
+      <th scope="col">Room</th>
+      <th scope="col">Capacity</th>
+      <th scope="col">Enrolled</th>
+    </tr>
+  </thead>
+  <tbody id="classes">
+
+   </tbody>
+</table>`;
+  $("#class-table").append(classTable);
+  for (var i = 0; i < object.length; i++) {
+    var newRow = `<tr>
+      <th scope="row">${object[i].Course.courseName}</th>
+      <td>${object[i].Professor.lastName}, ${object[i].Professor.firstName}</td>
+      <td>${object[i].begTime}</td>
+      <td>${object[i].days}</td>
+      <td>${object[i].building}</td>
+      <td>${object[i].room}</td>
+      <td>${object[i].maxNumStudents}</td>
+      <td>${object[i].currNumStudents}</td>
+    </tr>`;
+    $("#classes").append(newRow);
   }
 }
